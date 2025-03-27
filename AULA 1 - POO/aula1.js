ab76daf379df43fe327712ac0bc6b21073666a57
Rafael Ruhoff Gonçalves casas <rafaelruhoff@gmail.com>
2025-03-26 21:42:18 -0300

    Aula 1 POO

---
+++ AULA 1 - POO/aula1.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+class Cliente {
+    nome;
+    idade;
+    cpf;
+    agencia;
+}
+
+let cliente = new Cliente();
+
+cliente.nome = prompt("Digite o nome do cliente: ");
+cliente.idade = 21;
+cliente.cpf = '145.226.139-38';
+cliente.agencia = 2121;
+
+console.log(cliente);
+
