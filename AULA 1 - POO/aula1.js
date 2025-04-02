bab653a09b8b8364ac1c54f2c09a15116acb32af
Rafael Ruhoff Gonçalves casas <rafaelruhoff@gmail.com>
2025-04-02 20:54:24 -0300

    Update aula1.js

--- AULA 1 - POO/aula1.js
+++ AULA 1 - POO/aula1.js
@@ -2,15 +2,23 @@ const prompt = require('prompt-sync')();
 
 // Modelo de atributos e funções para criação de um objeto
 class Cliente {
-    nome; 
+    #nome; //Atributo
     idade;
     cpf;
     agencia;
+
+    get_nome() {
+        return this.#nome;
+    }
+
+    setNome(value){
+        this.#nome = value;
+    }
 }
 // Objeto que possui as infomações da classe de forma individual
 let cliente = new Cliente();
 
-cliente.nome = prompt("Digite o nome do cliente: ");
+cliente.setNome(prompt("Digite o nome do cliente: "));
 cliente.idade = 21;
 cliente.cpf = '145.226.139-38';
 cliente.agencia = 212129;
